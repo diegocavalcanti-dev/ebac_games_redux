462bb5d22f8dcdcf3178b337464888cdef701221
Diego Cavalcanti <cavalcantidiego@hotmail.com>
2024-10-09 19:08:42 -0300

    atualizando projeto

--- src/containers/Produtos.tsx
+++ src/containers/Produtos.tsx
@@ -1,19 +1,19 @@
 import { Game } from '../App'
 import Produto from '../components/Produto'
+import { useGetJogosQuery } from '../services/api'
 
 import * as S from './styles'
 
-type Props = {
-  jogos: Game[]
-  adicionarAoCarrinho: (jogo: Game) => void
-}
+const Produtos = () => {
+  const { data: jogos, isLoading } = useGetJogosQuery()
 
-const Produtos = ({ jogos, adicionarAoCarrinho }: Props) => {
+  if (isLoading) return <h2>Carregando</h2>
+
   return (
     <>
       <S.Produtos>
-        {jogos.map((game) => (
-          <Produto key={game.id} game={game} aoComprar={adicionarAoCarrinho} />
+        {jogos?.map((game) => (
+          <Produto key={game.id} game={game} />
         ))}
       </S.Produtos>
     </>
@@ -21,3 +21,40 @@ const Produtos = ({ jogos, adicionarAoCarrinho }: Props) => {
 }
 
 export default Produtos
+
+//
+
+// import { useEffect, useState } from 'react'
+// import { Game } from '../App'
+// import Produto from '../components/Produto'
+// import { useGetJogosQuery } from '../services/api'
+
+// import * as S from './styles'
+
+// const Produtos = () => {
+//   const { data: jogos, isLoading } = useGetJogosQuery()
+//   const [showLoading, setShowLoading] = useState(true)
+
+//   // useEffect para exibir o "Carregando" por 3 segundos
+//   useEffect(() => {
+//     const timer = setTimeout(() => {
+//       setShowLoading(false)
+//     }, 3000)
+
+//     return () => clearTimeout(timer) // Limpa o timer ao desmontar o componente
+//   }, [])
+
+//   if (isLoading || showLoading) return <h2>Carregando ...</h2>
+
+//   return (
+//     <>
+//       <S.Produtos>
+//         {jogos?.map((game) => (
+//           <Produto key={game.id} game={game} />
+//         ))}
+//       </S.Produtos>
+//     </>
+//   )
+// }
+
+// export default Produtos
